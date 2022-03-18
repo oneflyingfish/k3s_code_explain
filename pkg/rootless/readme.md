37398c063175cffc66b524f49db90633345a9e33
oneflyingfish <a_flying_fish@outlook.com>
2022-03-17 23:49:33 -0700

    explain rootless

---
+++ pkg/rootless/readme.md
@@ -0,0 +1,28 @@
+## RootlessKit介绍：
+
+> 推荐阅读：https://zhuanlan.zhihu.com/p/64661985
+> 
+> 有关`veth pair(Virtual Ethernet pair)`的内容: https://zhuanlan.zhihu.com/p/185686233
+>
+> rootless下的网络问题解决: https://www.sohu.com/a/405090353_354899
+
+* 类型: 基于用户命名空间的Linux fakeroot
+* 目标: 以非root用户运行Docker和Kubernetes ，从而保护主机上的真正根免受潜在的容器突破攻击，项目地址：https://github.com/rootless-containers/rootlesskit
+* 类似项目: fakeroot、fakeroot-ng、proot、become-root等等
+
+## 网络层转发方案:
+* host
+* **slirp4netns** (k3s选用的方案)
+* vpnkit
+* lxc-user-nic (实验性)
+
+> MTU=0指默认值，slirp4netns默认是65520
+
+## 端口转发方案:
+* none
+* **buildin** (k3s选用的方案)
+* slirp4netns
+
+## k3s的rootless包
+
+如果你是第一次接触rootless技术以及对容器技术的本质完全不了解，这个包的代码理解起来可能有一点晤涩难懂。一句话解释其实就是利用rootless技术，让程序在非root的情况下以虚假的root的形式运行起来(此时查看os.Getuid()==0)。
--- pkg/rootless/rootless.go
+++ pkg/rootless/rootless.go
@@ -23,6 +23,7 @@ var (
 	Sock     = ""
 )
 
+// 默认: stateDir= ${HOME}/.rancher/k3s
 func Rootless(stateDir string) error {
 	defer func() {
 		os.Unsetenv(pipeFD)
@@ -32,30 +33,41 @@ func Rootless(stateDir string) error {
 	hasFD := os.Getenv(pipeFD) != ""
 	hasChildEnv := os.Getenv(childEnv) != ""
 
+	// 注意，初次执行此函数，hasFD=false
 	if hasFD {
+		// $_K3S_ROOTLESS_FD存在
+		// 此时运行的是子进程
 		logrus.Debug("Running rootless child")
 		childOpt, err := createChildOpt()
 		if err != nil {
 			logrus.Fatal(err)
 		}
+
+		// 内部会接收parent发送的套接字信息，判断message，如果状态还未就绪会再次重启（直接通过syscall.exec替换当前进程）
+		// 状态就绪后，阻塞当前进程（实际上已经是子进程），启动孙子进程（/proc/self/exe os.args[1:]...），执行程序
 		if err := child.Child(*childOpt); err != nil {
 			logrus.Fatal("child died", err)
 		}
 	}
 
+	// 注意，初次执行次函数，hasChildEnv=false
 	if hasChildEnv {
+		// 此时子进程初始化成功
+		// $_K3S_ROOTLESS_SOCK存在
 		Sock = os.Getenv(childEnv)
 		logrus.Debug("Running rootless process")
 		return setupMounts(stateDir)
 	}
 
 	logrus.Debug("Running rootless parent")
-	parentOpt, err := createParentOpt(filepath.Join(stateDir, "rootless"))
+	parentOpt, err := createParentOpt(filepath.Join(stateDir, "rootless")) // ${HOME}/.rancher/k3s/rootless
 	if err != nil {
 		logrus.Fatal(err)
 	}
 
-	os.Setenv(childEnv, filepath.Join(parentOpt.StateDir, parent.StateFileAPISock))
+	os.Setenv(childEnv, filepath.Join(parentOpt.StateDir, parent.StateFileAPISock)) // $_K3S_ROOTLESS_SOCK={REST API Socket}
+	// 此处会阻塞，内部开始监听Sock，将以子进程运行 /proc/self/exe os.Args[1:]
+	// /proc/self/exe 为特殊路径，指向当前程序的路径
 	if err := parent.Parent(*parentOpt); err != nil {
 		logrus.Fatal(err)
 	}
@@ -83,7 +95,7 @@ func createParentOpt(stateDir string) (*parent.Opt, error) {
 		return nil, errors.Wrapf(err, "failed to mkdir %s", stateDir)
 	}
 
-	stateDir, err := ioutil.TempDir("", "rootless")
+	stateDir, err := ioutil.TempDir("", "rootless") // 在os.TempDir目录下创建以rootless为前缀的文件夹 (rootless$random)，返回值为新文件夹的path
 	if err != nil {
 		return nil, err
 	}
@@ -97,8 +109,11 @@ func createParentOpt(stateDir string) (*parent.Opt, error) {
 	if err != nil {
 		return nil, err
 	}
-	disableHostLoopback := true
+	disableHostLoopback := true // 即禁止连接主机命名空间的127.0.0.1:*
 	binary := "slirp4netns"
+
+	// 确保slirp4netns已经安装并配置环境变量
+	// 在环境变量中查找slirp4netns二进制文件，不存在直接返回错误，第一个返回值为 slirp4netns的path，例如: /bin/slirp4netns
 	if _, err := exec.LookPath(binary); err != nil {
 		return nil, err
 	}
